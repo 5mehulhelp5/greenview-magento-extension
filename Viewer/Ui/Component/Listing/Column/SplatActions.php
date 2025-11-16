e0e48b034ce33cd23dc5a76e2078c2dc14fa413a
anggaPixa <anggas25@live.com>
2025-11-17 03:09:57 +0700

    add action button in manage splats table, AR and Preview

--- Viewer/Ui/Component/Listing/Column/SplatActions.php
+++ Viewer/Ui/Component/Listing/Column/SplatActions.php
@@ -13,22 +13,31 @@ namespace GreenView\Viewer\Ui\Component\Listing\Column;
 use Magento\Framework\View\Element\UiComponent\ContextInterface;
 use Magento\Framework\View\Element\UiComponentFactory;
 use Magento\Ui\Component\Listing\Columns\Column;
+use GreenView\Viewer\Helper\Data as Helper;
 
 class SplatActions extends Column
 {
     /**
+     * @var Helper
+     */
+    protected $helper;
+
+    /**
      * @param ContextInterface $context
      * @param UiComponentFactory $uiComponentFactory
+     * @param Helper $helper
      * @param array $components
      * @param array $data
      */
     public function __construct(
         ContextInterface $context,
         UiComponentFactory $uiComponentFactory,
+        Helper $helper,
         array $components = [],
         array $data = []
     ) {
         parent::__construct($context, $uiComponentFactory, $components, $data);
+        $this->helper = $helper;
     }
 
     /**
@@ -40,22 +49,37 @@ class SplatActions extends Column
     public function prepareDataSource(array $dataSource)
     {
         if (isset($dataSource['data']['items'])) {
+            $companyToken = $this->helper->getCompanyToken();
+
             foreach ($dataSource['data']['items'] as & $item) {
-                if (isset($item['id'])) {
-                    $item[$this->getData('name')] = [
-                        'view' => [
-                            'label' => __('View Details'),
-                            'callback' => [
-                                [
-                                    'provider' => 'greenview_splats_listing.greenview_splats_listing.greenview_splats_columns.actions',
-                                    'target' => 'viewSplatDetails',
-                                    'params' => [
-                                        'id' => $item['id']
-                                    ]
-                                ]
-                            ]
-                        ]
-                    ];
+                if (isset($item['id']) && isset($item['slug'])) {
+                    $actions = [];
+
+                    // Preview button - opens in new tab
+                    if (!empty($item['slug']) && !empty($companyToken)) {
+                        $previewUrl = 'https://dashboard.green-view.nl/viewer/' .
+                                     urlencode($companyToken) . '/' .
+                                     urlencode($item['slug']);
+
+                        $actions['preview'] = [
+                            'href' => $previewUrl,
+                            'label' => __('Preview'),
+                            'target' => '_blank'
+                        ];
+
+                        // AR button - opens in new tab
+                        $arUrl = 'https://dashboard.green-view.nl/ar/' .
+                                urlencode($companyToken) . '/' .
+                                urlencode($item['slug']);
+
+                        $actions['ar'] = [
+                            'href' => $arUrl,
+                            'label' => __('AR'),
+                            'target' => '_blank'
+                        ];
+                    }
+
+                    $item[$this->getData('name')] = $actions;
                 }
             }
         }
